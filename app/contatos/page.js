c16deae3622b9fa34630607ab812b356701ee876
Fernando O. de Avellar Jr <favellar@yahoo.com.br>
2024-01-09 20:11:07 -0300

    ajuste semantico em contatos

--- app/contatos/page.js
+++ app/contatos/page.js
@@ -13,31 +13,29 @@ export default function ContatosPage() {
             height={254}
             alt="Imagem"
           ></Image>
-          <h3 className="mb-2 text-left text-2xl">Telefone</h3>
-          <p>
-            Estamos prontos para atendê-los: <br></br>
+          <h3 className="mb-2 text-left text-2xl">Telefones</h3>
+          <div>
+            <p>Estamos prontos para atendê-los:</p>
             <Link
               className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
               href="tel: 03534712123"
             >
-              (35) 3471-2123
+              <p>(35) 3471-2123</p>
             </Link>
-            <br></br>
             <Link
               href="tel: 03534714244"
               className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
             >
-              (35) 3471-4244
+              <p>(35) 3471-4244</p>
             </Link>
-            <br></br>
             <Link
               href="https://wa.me/5535988054435"
               className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
               target="_blank"
             >
-              (35) 98805-4435 (WhatsApp)
+              <p>(35) 98805-4435 (WhatsApp)</p>
             </Link>
-          </p>
+          </div>
         </div>
         <div className="overflow-hidden rounded border-4 border-solid border-white bg-white p-4">
           <Image
@@ -47,38 +45,36 @@ export default function ContatosPage() {
             height={254}
             alt="Imagem"
           ></Image>
-          <h3 className="mb-2 text-left text-2xl">Email</h3>
-          <p className="text-left">
-            Caso tenha interesse por um de nosssos produtos e/ou gostaria de
-            mais informações:
-            <br></br>
-            <a
+          <h3 className="mb-2 text-left text-2xl">Emails</h3>
+          <div className="text-left">
+            <p>
+              Caso tenha interesse por um de nosssos produtos e/ou gostaria de
+              mais informações:
+            </p>
+            <Link
               href="mailto:eletrovendas@eletroteste.com.br"
               className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
             >
-              eletrovendas@eletroteste.com.br
-            </a>
+              <p>eletrovendas@eletroteste.com.br</p>
+            </Link>
             <br></br>
-            <br></br>
-            Caso seja fornecedor:
-            <br></br>
-            <a
+            <p>Caso seja fornecedor:</p>
+            <Link
               href="mailto:eletrocompras@eletroteste.com.br"
               className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
             >
               eletrocompras@eletroteste.com.br
-            </a>
+            </Link>
             <br></br>
             <br></br>
-            Caso queira enviar seu Curriculo:
-            <br></br>
-            <a
+            <p>Caso queira enviar seu Curriculo:</p>
+            <Link
               href="mailto:rheletro@eletroteste.com.br"
               className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
             >
-              rheletro@eletroteste.com.br
-            </a>
-          </p>
+              <p>rheletro@eletroteste.com.br</p>
+            </Link>
+          </div>
         </div>
         <div className="overflow-hidden rounded border-4 border-solid border-white bg-white p-4">
           <Image
@@ -89,18 +85,21 @@ export default function ContatosPage() {
             alt="Imagem"
           ></Image>
           <h3 className="mb-2 text-left text-2xl">Endereço</h3>
-          <p>
-            <p>
+          <div>
+            <div>
               <Link
                 href="https://www.google.com/maps/place/ELETROTESTE+IND+E+COM+LTDA/@-22.246412,-45.708897,15z/data=!4m6!3m5!1s0x94cba2fdceb70c69:0x3d823c2cfddf278e!8m2!3d-22.246412!4d-45.708897!16s%2Fg%2F1tp28ctj?hl=pt-BR&entry=ttu"
                 target="_blank"
                 className="text-sm text-blue-500 hover:text-sky-700 hover:underline"
               >
-                Rua José Pinto Vilela, 474 - Centro - Santa Rita do Sapucaí - MG
-                - CEP: 37540-000
+                <br></br>
+                <p>
+                  Rua José Pinto Vilela, 474 - Centro - Santa Rita do Sapucaí -
+                  MG - CEP: 37540-000
+                </p>
               </Link>
-            </p>
-          </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- app/ui/produto.js
+++ app/ui/produto.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Produto({ produto }) {
   return (
@@ -13,11 +14,11 @@ export default function Produto({ produto }) {
       <h3 className="mb-2 text-left text-2xl">{produto.titulo}</h3>
       <h4 className="mb-2 text-left font-normal">{`Mod:(${produto.modelo})`}</h4>
       <p className="mb-2 justify-center text-sm">{produto.conteudo}</p>
-      <a href={produto.pdfRef} target="_blank">
+      <Link href={produto.pdfRef} target="_blank">
         <button className="mb-2 mt-2 rounded bg-sky-600 p-2 text-center text-sm text-white hover:bg-sky-700">
           Ver detalhes
         </button>
-      </a>
+      </Link>
     </div>
   )
 }
