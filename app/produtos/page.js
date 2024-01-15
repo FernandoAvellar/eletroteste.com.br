6071acc99f72e9b566eb51195ff3c746178b8604
Fernando O. de Avellar Jr <favellar@yahoo.com.br>
2024-01-14 21:01:09 -0300

    grid md agora em  2 colunas

--- app/produtos/page.js
+++ app/produtos/page.js
@@ -16,7 +16,7 @@ export default function ProdutosPage() {
       cat: 'isolamento',
       name: 'Isolação elétrica/ Dielétricos/ EPIs e EPCs',
     },
-    { cat: 'motores', name: 'Motores elétricos' },
+    { cat: 'motores', name: 'Motores Elétricos' },
     { cat: 'oleos', name: 'Óleos Isolantes' },
     { cat: 'reles', name: 'Reles' },
     { cat: 'telecom', name: 'Telecomunicações' },
@@ -48,47 +48,43 @@ export default function ProdutosPage() {
   return (
     <>
       <div className="mx-auto w-full max-w-6xl px-2 md:px-4">
-        <div>
-          <Listbox value={selectedCategory} onChange={handleFilterChange}>
-            <div className="relative">
-              <Listbox.Button className="relative w-full cursor-default rounded-md border-sky-600 bg-gray-100 py-2 pl-3 pr-10 text-left text-sm shadow-lg outline-none ring-1 focus:outline-none focus-visible:border-sky-500 focus-visible:ring-1 focus-visible:ring-offset-2  md:text-base lg:text-lg">
-                <span className="block truncate">{selectedCategory.name}</span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                  <ChevronUpDownIcon
-                    className="size-5 text-gray-500"
-                    aria-hidden="true"
-                  />
-                </span>
-              </Listbox.Button>
-              <Transition
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Listbox.Options className="absolute mt-1 h-fit w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black/5 focus:outline-none md:text-base">
-                  {categorias.map((categoria) => (
-                    <Listbox.Option
-                      key={categoria.cat}
-                      className={({ active }) =>
-                        `relative cursor-default select-none py-2 pl-4 pr-4 ${
-                          active ? 'bg-sky-500 text-white' : 'text-gray-900'
-                        }`
-                      }
-                      value={categoria}
-                    >
-                      <span className="block truncate font-normal">
-                        {categoria.name}
-                      </span>
-                    </Listbox.Option>
-                  ))}
-                </Listbox.Options>
-              </Transition>
-            </div>
-          </Listbox>
-        </div>
+        <Listbox value={selectedCategory} onChange={handleFilterChange}>
+          <div className="relative">
+            <Listbox.Button className="relative w-full cursor-default rounded-md border-sky-600 bg-gray-100 py-2 pl-3 pr-10 text-left text-sm shadow-lg outline-none ring-1 focus:outline-none focus-visible:border-sky-500 focus-visible:ring-1 focus-visible:ring-offset-2  md:text-base lg:text-lg">
+              <span className="block truncate">{selectedCategory.name}</span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon
+                  className="size-5 text-gray-500"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute mt-1 h-fit w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black/5 focus:outline-none md:text-base">
+                {categorias.map((categoria) => (
+                  <Listbox.Option
+                    key={categoria.cat}
+                    value={categoria}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-4 pr-4 ${
+                        active ? 'bg-sky-500 text-white' : 'text-gray-900'
+                      }`
+                    }
+                  >
+                    <span>{categoria.name}</span>
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
       </div>
-      <div className="container mx-auto grid max-w-6xl gap-6 px-2 py-1 md:grid-cols-3 md:px-4 lg:px-4">
+      <div className="container mx-auto grid max-w-6xl gap-6 px-2 py-1 md:grid-cols-2 md:px-4 lg:grid-cols-3 lg:px-4">
         {produtosFiltrados.map((produto) => {
           return <Produto key={produto.id} produto={produto} />
         })}
