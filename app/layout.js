cea3db24c5a8060493a43ba54130fb8f35f5fafd
Fernando Avellar Jr <favellar@yahoo.com.br>
2024-01-26 17:00:56 -0300

    adicionado vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Header from './ui/header'
 import Footer from './ui/footer'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
